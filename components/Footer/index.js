37f2d80047e081aaff3a7d619b9e2f17c5b43f4e
Felipe <felipedantas184@gmail.com>
2021-12-20 19:08:27 -0300

    footer edit

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -13,67 +13,6 @@ const Footer = () => {
   return (
     <FooterContainer>
       <FooterWrap>
-        <FooterLinkContainer>
-          <FooterLinkWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle>Página Inicial</FooterLinkTitle>
-                <LinkS to="teachers">
-									<FooterLink>Professores</FooterLink>
-								</LinkS>
-	              <LinkS to="highlights">
-									<FooterLink>Diferenciais</FooterLink>
-								</LinkS>
-	              <LinkS to="perks">
-									<FooterLink>Vantagens</FooterLink>
-								</LinkS>
-	              <LinkS to="pricing">
-									<FooterLink>Preços</FooterLink>
-								</LinkS>
-	          </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle>Junte-se a Nós</FooterLinkTitle>
-                <Link href="/start-enem" passHref>
-									<FooterLink>Curso Start ENEM</FooterLink>
-								</Link>
-	              <ExtLink href="mailto:meuprof.contato@gmail.com" target='_blank' rel="noreferrer" aria-label='Email'>
-									<FooterLink>Mande um e-mail</FooterLink>
-								</ExtLink>
-	              <Link href="/" passHref>
-									<FooterLink>Apoie-nos</FooterLink>
-								</Link>
-	          </FooterLinkItems>
-          </FooterLinkWrapper>
-
-          <FooterLinkWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle>Sobre Nós</FooterLinkTitle>
-                <Link href="/como-funciona" passHref >
-									<FooterLink>Como Funciona</FooterLink>
-								</Link>
-	              <ExtLink href="https://www.instagram.com/meu.prof/" target='_blank' rel="noreferrer" >
-									<FooterLink>Quem somos</FooterLink>
-								</ExtLink>
-	              <Link href="/start-enem" passHref >
-									<FooterLink>Projetos</FooterLink>
-								</Link>
-	          </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle>Social Media</FooterLinkTitle>
-                <ExtLink href="https://www.instagram.com/meu.prof/" target='_blank' rel="noreferrer" aria-label='Instagram'>
-									<FooterLink>Instagram</FooterLink>
-								</ExtLink>
-	              <ExtLink href="https://www.facebook.com/MeuProf.THE/" target='_blank' rel="noreferrer" aria-label='Facebook'>
-									<FooterLink>Facebook</FooterLink>
-								</ExtLink>
-	              <ExtLink href="mailto:meuprof.contato@gmail.com" target='_blank' rel="noreferrer" aria-label='Email'>
-									<FooterLink>Email</FooterLink>
-								</ExtLink>
-	              <ExtLink href="https://twitter.com/MeuProf_/" target='_blank' rel="noreferrer" aria-label='Twitter'>
-									<FooterLink>Twitter</FooterLink>
-								</ExtLink>
-	          </FooterLinkItems>
-          </FooterLinkWrapper>
-        </FooterLinkContainer>
         <SocialMedia>
           <SocialMediaWrap>
             <Link href='/' passHref onClick={toggleHome}>
