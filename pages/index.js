9ef479e570354a7eef2604b3f5c73360958655d6
Felipe <felipedantas184@gmail.com>
2022-01-18 11:19:55 -0300

    add head import

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,19 @@ export default function Home() {
     <>
       <Head>
         <title>A.A.A. Carcará | Medicina UFPI</title>
+        <meta name="description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        
+        <meta property="og:title" content="A.A.A. Carcará | Medicina UFPI"/>
+        <meta property="og:type" content="school"/>
+        <meta property="og:description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013"/>
+        <meta property="og:image" content="/apple-touch-icon.png"/>
+        <meta property="og:site_name" content="A.A.A. Carcará"/>
+
+        <meta property="twitter:title" content="A.A.A. Carcará | Medicina UFPI"/>
+        <meta property="twitter:url" content="https://carcara.vercel.app/"/>
+        <meta property="twitter:description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013"/>
+        <meta property="twitter:image" content="/apple-touch-icon.png"/>
       </Head>
 
       <Hero />
