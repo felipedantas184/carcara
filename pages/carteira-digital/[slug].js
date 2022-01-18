5915e3d1de1467773622be0e28091c1ec3034acf
Felipe <felipedantas184@gmail.com>
2022-01-18 11:15:45 -0300

    title and description

--- pages/carteira-digital/[slug].js
+++ pages/carteira-digital/[slug].js
@@ -86,6 +86,23 @@ export default function RecipeDetails({ member }) {
     
     return (
       <>
+        <Head>
+          <title>Carteira Digital | A.A.A. Carcará</title>
+          <meta name="description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013" />
+          <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+          
+          <meta property="og:title" content="Carteira Digital | A.A.A. Carcará | Medicina UFPI"/>
+          <meta property="og:type" content="school"/>
+          <meta property="og:description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013"/>
+          <meta property="og:image" content="/apple-touch-icon.png"/>
+          <meta property="og:site_name" content="A.A.A. Carcará"/>
+
+          <meta property="twitter:title" content="Carteira Digital | A.A.A. Carcará | Medicina UFPI"/>
+          <meta property="twitter:url" content="https://carcara.vercel.app/"/>
+          <meta property="twitter:description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013"/>
+          <meta property="twitter:image" content="/apple-touch-icon.png"/>
+        </Head>
+
         <Card member={member} />
         <Discounts />
       </>
